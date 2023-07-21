bd24799bb1c56cfb7944ac50bd8ed08d09c56ed5
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-21 11:04:45 -0600

    Feat: Create Food poro

---
+++ app/models/poros/food.rb
@@ -0,0 +1,13 @@
+class Food
+  attr_reader :upc,
+              :description,
+              :brand_owner,
+              :ingredients
+
+  def initialize(food_data)
+    @upc = food_data[:gtinUpc]
+    @description = food_data[:description]
+    @brand_owner = food_data[:brandOwner]
+    @ingredients = food_data[:ingredients]
+  end
+end
